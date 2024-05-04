8ffacc0785295b32397d5c4cd2d6e16bf19002a3
FlyingWalross <jakob.jandl@gmail.com>
2024-05-05 00:13:56 +0200

    Remove request logger (could impact performance)

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const express = require('express');
-const logger = require('morgan');
 
 const apiRouter = require('./routes/api');
 
 const app = express();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api', apiRouter);
+
 const port = 8080;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
 
 module.exports = app;
